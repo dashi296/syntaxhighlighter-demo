e3fb268735c33763e54be9112f4c0a523ada3d97
unio <unioblog@gmail.com>
2019-11-06 16:08:03 +0900

    UI等微修正

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,16 +1,9 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState } from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
-import HighlightedItem from './HighlightedItem';
-import hljsStyles from './hljsStyles';
-import prismStyles from './prismStyles';
-import hljsLanguages from './hljsLanguages';
-import prismLanguages from './prismLanguages';
-
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
@@ -20,17 +13,21 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormLabel from '@material-ui/core/FormLabel';
 
 import Grid from '@material-ui/core/Grid';
+import prismLanguages from './prismLanguages';
+import hljsLanguages from './hljsLanguages';
+import prismStyles from './prismStyles';
+import hljsStyles from './hljsStyles';
+import HighlightedItem from './HighlightedItem';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-
-    },
+    root: {},
     container: {
       display: 'flex',
       flexWrap: 'wrap',
     },
     textField: {
+      width: '80%',
       marginLeft: theme.spacing(1),
       marginRight: theme.spacing(1),
     },
@@ -47,23 +44,27 @@ const useStyles = makeStyles((theme: Theme) =>
     selectEmpty: {
       marginTop: theme.spacing(2),
     },
+    separater: {
+      padding: 0,
+      margin: 20,
+      borderTop: '1px solid #CCCCCC',
+    },
   }),
 );
 
-export interface IDisplayProps{
+type highlighterType = 'hljs' | 'prism';
+const defaultLanguage = 'ruby';
 
-}
-
-type highlighterType = "hljs" | "prism";
-const defaultLanguage = "ruby";
-
-const Display: FC<IDisplayProps> = ({
-
-}) => {
+const Display: FC = () => {
   const classes = useStyles();
   const [highlighter, setHighlighter] = useState<highlighterType>('hljs');
-  const [code, setCode] = useState('(num) => num + 1');
-  const [language, setLanguage] = useState('ruby');
+  const [code, setCode] = useState(`def add(num)
+  num += 1
+end`);
+  const [selectedLanguage, setLanguage] = useState('ruby');
+
+  let languages = highlighter === 'hljs' ? hljsLanguages : prismLanguages;
+  let styles = highlighter === 'hljs' ? hljsStyles : prismStyles;
 
   const handleLanguage = (event: React.ChangeEvent<{ value: unknown }>) => {
     setLanguage(event.target.value as string);
@@ -71,68 +72,92 @@ const Display: FC<IDisplayProps> = ({
 
   const handleHighlighter = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
-    const highlighter:highlighterType = event.target.value == "hljs" ? "hljs" : "prism";
-    const languages: string[] = highlighter == "hljs" ? hljsLanguages : prismLanguages;
-    const newLanguage:string = languages.includes(language) ? language : defaultLanguage;
-    setHighlighter(highlighter);
+    const newHighlighter: highlighterType =
+      event.target.value === 'hljs' ? 'hljs' : 'prism';
+    languages = newHighlighter === 'hljs' ? hljsLanguages : prismLanguages;
+    styles = newHighlighter === 'hljs' ? hljsStyles : prismStyles;
+    const newLanguage = languages.includes(selectedLanguage)
+      ? selectedLanguage
+      : defaultLanguage;
+    setHighlighter(newHighlighter);
     setLanguage(newLanguage);
   };
 
-  let languages = highlighter == "hljs" ? hljsLanguages : prismLanguages;
-  let styles = highlighter == "hljs" ? hljsStyles : prismStyles;
-
-  useEffect(()=>{
-    languages = highlighter == "hljs" ? hljsLanguages : prismLanguages;
-    styles = highlighter == "hljs" ? hljsStyles : prismStyles;
-  }, [highlighter])
-
   return (
     <div className={classes.root}>
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <FormControl component="fieldset" className={classes.formControl}>
+            <FormLabel component="legend">Highlighter</FormLabel>
+            <RadioGroup
+              aria-label="gender"
+              name="gender1"
+              value={highlighter}
+              onChange={handleHighlighter}
+            >
+              <FormControlLabel value="hljs" control={<Radio />} label="hljs" />
+              <FormControlLabel
+                value="prism"
+                control={<Radio />}
+                label="prism"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
 
-      <TextField
-        id="codeInput"
-        label="Code"
-        multiline
-        rowsMax="10"
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
-        className={classes.textField}
-        margin="normal"
-        variant="filled"
-      />
+        <Grid item xs={2}>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="language-select-label">Language</InputLabel>
+            <Select
+              labelId="language-select-label"
+              id="language-select"
+              value={selectedLanguage}
+              onChange={handleLanguage}
+            >
+              {languages.map(language => (
+                <MenuItem key={language} value={language}>
+                  {language}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={8} />
 
-      <FormControl component="fieldset" className={classes.formControl}>
-        <FormLabel component="legend">Highlighter</FormLabel>
-        <RadioGroup aria-label="gender" name="gender1" value={highlighter} onChange={handleHighlighter}>
-          <FormControlLabel value={"hljs"} control={<Radio />} label="hljs" />
-          <FormControlLabel value={"prism"} control={<Radio />} label="prism" />
-        </RadioGroup>
-      </FormControl>
-
-      <FormControl className={classes.formControl}>
-        <InputLabel id="language-select-label">Language</InputLabel>
-        <Select
-          labelId="language-select-label"
-          id="language-select"
-          value={language}
-          onChange={handleLanguage}
-        >
-          { languages.map(language => <MenuItem key={language} value={language}>{language}</MenuItem>) }
-        </Select>
-      </FormControl>
-
-      <Grid container spacing={2} justify="flex-start">
-      {Object.keys(styles).map(styleName => (
-        <Grid item key={styleName} xs={12} sm={6} md={6} lg={4}>
-          <HighlightedItem styleName={styleName} type={highlighter} language={language} style={styles[styleName]} code={code} />
-        </Grid>))
-      }
+        <Grid item xs={12} sm={6}>
+          <div>
+            <TextField
+              id="codeInput"
+              label="code"
+              multiline
+              rows={10}
+              rowsMax={10}
+              value={code}
+              onChange={e => setCode(e.target.value)}
+              className={classes.textField}
+              variant="outlined"
+            />
+          </div>
+        </Grid>
       </Grid>
 
+      <div className={classes.separater} />
+
+      <Grid container spacing={2} justify="flex-start">
+        {Object.keys(styles).map(styleName => (
+          <Grid item key={styleName} xs={12} sm={6} md={6} lg={4}>
+            <HighlightedItem
+              styleName={styleName}
+              type={highlighter}
+              language={selectedLanguage}
+              style={styles[styleName]}
+              code={code}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </div>
-
-
-  )
-}
+  );
+};
 
 export default Display;
